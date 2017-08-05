cf31c0a7316aafb10b460e887fd3594b2f24f619
kawasemi <a-tomohiro@sumilab.org>
2017-08-05 13:19:43 +0900

    loading and processing

--- test.py
+++ test.py
@@ -2,6 +2,25 @@ files = ['data/log1.txt','data/log2.txt']
 table = {}
 step = 0
 
+def sourceProcessingStep(f):
+    for line in open(f, 'r+'):
+        line = line.rstrip().split('\t')
+        key = tuple(line[0:4])
+        if key not in table:
+            table[key] = [[float(v) for v in line[4:]],[]]
+
+def comparisonStep(f):
+    for line in open(f, 'r+'):
+        line = line.rstrip().split('\t')
+        key = tuple(line[0:4])
+        if key in table:
+            table[key][1] = [float(v) for v in line[4:]]
+        else:
+            print('the columm does not match')
+
+def printResilt():
+    print('result')
+
 for f in files:
     print(f)
     if step == 0:
@@ -12,16 +31,4 @@ for f in files:
     step+=1
     print(table)
 
-def sourceProcessingStep(f):
-    for line in open(f, 'r+'):
-        line = line.rstrip().split('\t')
-        key = tuple(line[0:4])
-        if key not in table:
-            table[key] = [line[5:],[]]
-
-def comparisonStep(f):
-    for line in open(f, 'r+'):
-        line = line.rstrip().split('\t')
-        key = tuple(line[0:4])
-        if key not in table:
-            table[key] = [line[5:],[]]
+printResilt()
